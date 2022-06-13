71c4a20feed9a3137651d5fd5430671cfb7aa0ab
field <jsw_777@naver.com>
2022-06-13 12:39:13 +0900

    Initial Project

---
+++ src/main/java/site/orangefield/restdoc/RestDocApplication.java
@@ -0,0 +1,13 @@
+package site.orangefield.restdoc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestDocApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestDocApplication.class, args);
+	}
+
+}
